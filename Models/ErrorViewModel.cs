25905668ad093578d40f6306dc7307f61e1c6ae3
Andrecito24 <oscar_febres@usmp.pe>
2024-04-27 09:53:28 -0500

    Primer Commit Proyecto

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace appbanconuevoauth.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
